ff23420cd69946e0864a8d1e2d5a868bc7f679ea
david <david.barinas.dev@gmail.com>
2018-08-22 22:46:48 -0500

    add needed libraries and test with babel-node

--- api/config/datastores.js
+++ api/config/datastores.js
@@ -15,7 +15,6 @@
 
 module.exports.datastores = {
 
-
   /***************************************************************************
   *                                                                          *
   * Your app's default datastore.                                            *
@@ -33,25 +32,11 @@ module.exports.datastores = {
   ***************************************************************************/
 
   default: {
-
-    /***************************************************************************
-    *                                                                          *
-    * Want to use a different database during development?                     *
-    *                                                                          *
-    * 1. Choose an adapter:                                                    *
-    *    https://sailsjs.com/plugins/databases                                 *
-    *                                                                          *
-    * 2. Install it as a dependency of your Sails app.                         *
-    *    (For example:  npm install sails-mysql --save)                        *
-    *                                                                          *
-    * 3. Then pass it in, along with a connection URL.                         *
-    *    (See https://sailsjs.com/config/datastores for help.)                 *
-    *                                                                          *
-    ***************************************************************************/
-    // adapter: 'sails-mysql',
-    // url: 'mysql://user:password@host:port/database',
-
+      adapter: 'sails-mysql',
+      host: 'localhost',
+      database: 'contactsDbTest',
+      port: 3306,
+      user: 'root',
+      password: ''
   },
-
-
 };
